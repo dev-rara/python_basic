0d169c25c66a668b6d8909acf48d705360221b02
rara_dev <kksr1219@gmail.com>
2024-06-12 15:58:54 +0900

    20240612 study

---
+++ 20240612_2/module.py
@@ -0,0 +1,1 @@
+import addModuleimport subModuleimport mulModuleimport divModuledef mod(n1, n2):    return n1 % n2if __name__ == '__main__':    print(addModule.add(10, 20))    print(subModule.sub(10, 20))    print(mulModule.mul(10, 20))    print(divModule.div(10, 20))
